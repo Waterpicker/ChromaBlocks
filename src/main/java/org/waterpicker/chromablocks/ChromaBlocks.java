e952b5d60d3fb8e7835a704e4adda47db254a6db
Waterpicker <Waterpickerenternity@gmail.com>
2018-07-23 05:15:42 -0500

    Added White Chroma Slab

--- src/main/java/org/waterpicker/chromablocks/ChromaBlocks.java
+++ src/main/java/org/waterpicker/chromablocks/ChromaBlocks.java
@@ -1,6 +1,7 @@
 package org.waterpicker.chromablocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockSlab;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
@@ -14,6 +15,13 @@ import java.util.function.Supplier;
 
 public class ChromaBlocks implements BlockAdder, ItemAdder {
     private static Supplier<Block> CHROMA_BLOCK = () -> new Block(Block.Builder.create(Material.IRON)) {
+        @Override
+        public int getLightValue(IBlockState p_getLightValue_1_) {
+            return 15;
+        }
+    };
+
+    private static Supplier<BlockSlab> CHROMA_SLAB = () -> new BlockSlab(Block.Builder.create(Material.IRON)) {
         @Override
         public int getLightValue(IBlockState p_getLightValue_1_) {
             return 15;
@@ -37,24 +45,28 @@ public class ChromaBlocks implements BlockAdder, ItemAdder {
     public static Block CHROMA_WHITE = CHROMA_BLOCK.get();
     public static Block CHROMA_YELLOW = CHROMA_BLOCK.get();
 
+    public static BlockSlab CHROMA_WHITE_SLAB = CHROMA_SLAB.get();
+
     @Override
     public void registerBlocks() {
         Block.registerBlock("chromablocks:chroma_black", CHROMA_BLACK);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_blue"), CHROMA_BLUE);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_brown"), CHROMA_BROWN);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_cyan"), CHROMA_CYAN);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_gray"), CHROMA_GRAY);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_green"), CHROMA_GREEN);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_light_blue"), CHROMA_LIGHT_BLUE);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_light_gray"), CHROMA_SILVER);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_lime"), CHROMA_LIME);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_magenta"), CHROMA_MAGENTA);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_orange"), CHROMA_ORANGE);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_pink"), CHROMA_PINK);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_purple"), CHROMA_PURPLE);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_red"), CHROMA_RED);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_white"), CHROMA_WHITE);
-        Block.registerBlock(new ResourceLocation("chromablocks:chroma_yellow"), CHROMA_YELLOW);
+        Block.registerBlock("chromablocks:chroma_blue", CHROMA_BLUE);
+        Block.registerBlock("chromablocks:chroma_brown", CHROMA_BROWN);
+        Block.registerBlock("chromablocks:chroma_cyan", CHROMA_CYAN);
+        Block.registerBlock("chromablocks:chroma_gray", CHROMA_GRAY);
+        Block.registerBlock("chromablocks:chroma_green", CHROMA_GREEN);
+        Block.registerBlock("chromablocks:chroma_light_blue", CHROMA_LIGHT_BLUE);
+        Block.registerBlock("chromablocks:chroma_light_gray", CHROMA_SILVER);
+        Block.registerBlock("chromablocks:chroma_lime", CHROMA_LIME);
+        Block.registerBlock("chromablocks:chroma_magenta", CHROMA_MAGENTA);
+        Block.registerBlock("chromablocks:chroma_orange", CHROMA_ORANGE);
+        Block.registerBlock("chromablocks:chroma_pink", CHROMA_PINK);
+        Block.registerBlock("chromablocks:chroma_purple", CHROMA_PURPLE);
+        Block.registerBlock("chromablocks:chroma_red", CHROMA_RED);
+        Block.registerBlock("chromablocks:chroma_white", CHROMA_WHITE);
+        Block.registerBlock("chromablocks:chroma_yellow", CHROMA_YELLOW);
+
+        Block.registerBlock("chromablocks:chroma_white_slab", CHROMA_WHITE_SLAB);
     }
 
     @Override
@@ -75,5 +87,7 @@ public class ChromaBlocks implements BlockAdder, ItemAdder {
         Item.registerItemBlock(CHROMA_RED, ItemGroup.BUILDING_BLOCKS);
         Item.registerItemBlock(CHROMA_WHITE, ItemGroup.BUILDING_BLOCKS);
         Item.registerItemBlock(CHROMA_YELLOW, ItemGroup.BUILDING_BLOCKS);
+
+        Item.registerItemBlock(CHROMA_WHITE_SLAB, ItemGroup.BUILDING_BLOCKS);
     }
 }
